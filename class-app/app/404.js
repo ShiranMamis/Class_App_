45d79ad5d091cfc06dbc2ce9467ba090bf470c0c
Shiran Mamistvalov <160395215+ShiranMamis@users.noreply.github.com>
2025-10-07 16:22:08 +0300

    Delete class-app directory

--- class-app/app/404.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function notFound() {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <h1>עמוד זה אינו נמצא</h1>
-    </div>
-  );
-}
